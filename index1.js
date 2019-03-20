ec526b7bcef5c790943385459b506979b87d4564
Dani Vicario <danigv.email@gmail.com>
2019-03-20 10:05:08 +0100

    added object exploration

---
+++ index1.js
@@ -0,0 +1,14 @@
+let employee = {
+    name : "sandra",
+    age: 24,
+    salary: 30000
+}
+
+employee.salary *= 1.16
+
+
+
+employee.department = "admin"
+delete employee.age
+
+console.log(employee)
